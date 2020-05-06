501d756fc7cd40d705ded9e21afbf08ea87b366b
melk0sha <din.ivanova13@gmail.com>
2020-05-07 00:36:17 +0300

    feat: add constants

---
+++ src/resources/constants.js
@@ -0,0 +1,16 @@
+export const CANVAS_RESOLUTION = {
+  RES_512: { width: 512, height: 512 },
+  RES_256: { width: 256, height: 256 },
+  RES_128: { width: 128, height: 128 },
+};
+
+export const TOOLS = {
+  BRUSH: "Brush",
+  ERASER: "Eraser",
+};
+
+export const EVENTS = {
+  ONMOUSEDOWN: "onmousedown",
+  ONMOUSEMOVE: "onmousemove",
+  ONMOUSEUP: "onmouseup",
+};
