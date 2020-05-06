a6b638612c0e4328a13c2af28a386e2bc769c3f7
melk0sha <din.ivanova13@gmail.com>
2020-05-07 00:37:09 +0300

    feat: add app.js, state for app and some btns with layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import React from "react";
+import React, { Component } from "react";
+import Canvas from "./components/Canvas";
+import Tool from "./components/Tool";
+import onBrush from "./utils/onBrush";
+import onErase from "./utils/onErase";
+import { CANVAS_RESOLUTION, TOOLS } from "./resources/constants";
+import "./index.scss";
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    canvasSettings: { canvas: {}, ctx: {}, canvasResolution: {} },
+    currentTool: TOOLS.BRUSH,
+  };
+
+  componentDidMount() {
+    const canvasElement = document.getElementById("canvas");
+
+    this.setState({
+      canvasSettings: {
+        canvas: canvasElement,
+        ctx: canvasElement.getContext("2d"),
+        canvasResolution: CANVAS_RESOLUTION.RES_512,
+      },
+    });
+  }
+
+  onToolAction = (e) => {
+    const { currentTool } = this.state;
+
+    switch (currentTool) {
+      default:
+      case TOOLS.BRUSH:
+        onBrush(e);
+        break;
+      case TOOLS.ERASER:
+        onErase(e);
+        break;
+    }
+  };
+
+  onToolChange = ({ target: { textContent: currentTool } }) => {
+    this.setState({ currentTool });
+  };
+
+  render() {
+    const { onToolAction, onToolChange } = this;
+    const {
+      canvasSettings: { canvasResolution },
+      currentTool,
+    } = this.state;
+
+    return (
+      <div className="wrapper">
+        <div className="tools">
+          <Tool
+            type={TOOLS.BRUSH}
+            onToolChange={onToolChange}
+            currentTool={currentTool}
+          />
+          <Tool
+            type={TOOLS.ERASER}
+            onToolChange={onToolChange}
+            currentTool={currentTool}
+          />
+        </div>
+        <Canvas
+          canvasResolution={canvasResolution}
+          onToolAction={onToolAction}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
