4a58a3b6bb955ea015b97eb11bceed0de8ec84ae
melk0sha <din.ivanova13@gmail.com>
2020-05-07 17:19:02 +0300

    feat: add brushing and erasing implementations

--- src/utils/onBrush.js
+++ src/utils/onBrush.js
@@ -1,13 +1,17 @@
 import getCursorCoordinates from "./getCursorCoordinates";
+import getCanvasStyleResolution from "./getCanvasStyleResolution";
 import { EVENTS } from "../resources/constants";
 
-const onBrush = ({ type: eventType }) => {
-  switch (eventType) {
+let isDrawing = false;
+let previousPosition;
+
+const onBrush = (e, canvasSettings) => {
+  switch (e.type) {
     case EVENTS.ONMOUSEDOWN:
-      onBrushMouseDown();
+      onBrushMouseDown(e, canvasSettings);
       break;
     case EVENTS.ONMOUSEMOVE:
-      onBrushMouseMove();
+      onBrushMouseMove(e, canvasSettings);
       break;
     case EVENTS.ONMOUSEUP:
       onBrushMouseUp();
@@ -17,8 +21,62 @@ const onBrush = ({ type: eventType }) => {
   }
 };
 
-const onBrushMouseDown = () => {};
-const onBrushMouseMove = () => {};
-const onBrushMouseUp = () => {};
+const onBrushMouseDown = (e, canvasSettings) => {
+  isDrawing = true;
+  previousPosition = getCursorCoordinates(e);
+  brush(previousPosition, canvasSettings);
+};
+
+const onBrushMouseMove = (e, canvasSettings) => {
+  if (isDrawing) {
+    brushWithoutSkipping(
+      previousPosition,
+      getCursorCoordinates(e),
+      canvasSettings
+    );
+    previousPosition = getCursorCoordinates(e);
+  }
+};
+
+const onBrushMouseUp = () => {
+  isDrawing = false;
+};
+
+const brush = (position, { canvas, ctx, canvasResolution }) => {
+  let [x, y] = position;
+  const canvasStyleResolution = getCanvasStyleResolution(canvas);
+
+  x = Math.floor(x / (canvasStyleResolution.width / canvasResolution.width));
+  y = Math.floor(y / (canvasStyleResolution.height / canvasResolution.height));
+
+  ctx.fillStyle = "#000000";
+  ctx.fillRect(x, y, 3, 3);
+};
+
+const brushWithoutSkipping = (prevPosition, newPosition, canvasSettings) => {
+  let [x0, y0] = prevPosition;
+  const [x1, y1] = newPosition;
+  const dx = Math.abs(x1 - x0);
+  const dy = Math.abs(y1 - y0);
+  const sx = x0 < x1 ? 1 : -1;
+  const sy = y0 < y1 ? 1 : -1;
+  let err = dx - dy;
+
+  while (true) {
+    brush([x0, y0], canvasSettings);
+    if (x0 === x1 && y0 === y1) {
+      break;
+    }
+    const e2 = 2 * err;
+    if (e2 > -dy) {
+      err -= dy;
+      x0 += sx;
+    }
+    if (e2 < dx) {
+      err += dx;
+      y0 += sy;
+    }
+  }
+};
 
 export default onBrush;
--- src/utils/onErase.js
+++ src/utils/onErase.js
@@ -1,13 +1,17 @@
 import getCursorCoordinates from "./getCursorCoordinates";
+import getCanvasStyleResolution from "./getCanvasStyleResolution";
 import { EVENTS } from "../resources/constants";
 
-const onErase = ({ type: eventType }) => {
-  switch (eventType) {
+let isErasing = false;
+let previousPosition;
+
+const onErase = (e, canvasSettings) => {
+  switch (e.type) {
     case EVENTS.ONMOUSEDOWN:
-      onEraseMouseDown();
+      onEraseMouseDown(e, canvasSettings);
       break;
     case EVENTS.ONMOUSEMOVE:
-      onEraseMouseMove();
+      onEraseMouseMove(e, canvasSettings);
       break;
     case EVENTS.ONMOUSEUP:
       onEraseMouseUp();
@@ -17,8 +21,61 @@ const onErase = ({ type: eventType }) => {
   }
 };
 
-const onEraseMouseDown = () => {};
-const onEraseMouseMove = () => {};
-const onEraseMouseUp = () => {};
+const onEraseMouseDown = (e, canvasSettings) => {
+  isErasing = true;
+  previousPosition = getCursorCoordinates(e);
+  erase(previousPosition, canvasSettings);
+};
+
+const onEraseMouseMove = (e, canvasSettings) => {
+  if (isErasing) {
+    eraseWithoutSkipping(
+      previousPosition,
+      getCursorCoordinates(e),
+      canvasSettings
+    );
+    previousPosition = getCursorCoordinates(e);
+  }
+};
+
+const onEraseMouseUp = () => {
+  isErasing = false;
+};
+
+const erase = (position, { canvas, ctx, canvasResolution }) => {
+  let [x, y] = position;
+  const canvasStyleResolution = getCanvasStyleResolution(canvas);
+
+  x = Math.floor(x / (canvasStyleResolution.width / canvasResolution.width));
+  y = Math.floor(y / (canvasStyleResolution.height / canvasResolution.height));
+
+  ctx.clearRect(x, y, 3, 3);
+};
+
+const eraseWithoutSkipping = (prevPosition, newPosition, canvasSettings) => {
+  let [x0, y0] = prevPosition;
+  const [x1, y1] = newPosition;
+  const dx = Math.abs(x1 - x0);
+  const dy = Math.abs(y1 - y0);
+  const sx = x0 < x1 ? 1 : -1;
+  const sy = y0 < y1 ? 1 : -1;
+  let err = dx - dy;
+
+  while (true) {
+    erase([x0, y0], canvasSettings);
+    if (x0 === x1 && y0 === y1) {
+      break;
+    }
+    const e2 = 2 * err;
+    if (e2 > -dy) {
+      err -= dy;
+      x0 += sx;
+    }
+    if (e2 < dx) {
+      err += dx;
+      y0 += sy;
+    }
+  }
+};
 
 export default onErase;
